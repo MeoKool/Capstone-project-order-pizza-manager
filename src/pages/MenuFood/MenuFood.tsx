4131532fc2b91855d777a192d357d4d9a94117b3
MeoKool <truongsyquang1202@gmail.com>
2025-03-13 04:23:14 +0700

    feat: add alert dialog component for delete confirmation; refactor MenuFood to include header and food list

--- src/pages/MenuFood/MenuFood.tsx
+++ src/pages/MenuFood/MenuFood.tsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'
-import { AddFoodDialog } from './components/AddFoodDialog'
-import { Button } from '@/components/ui/button'
+import React from 'react'
+import { MenuFoodHeader } from './components/Header'
+import FoodList from './components/FoodList'
 
 const MenuFood: React.FC = () => {
-  const [dialogOpen, setDialogOpen] = useState(false)
-
   return (
     <div>
-      <Button onClick={() => setDialogOpen(true)}>Thêm món ăn mới</Button>
+      <MenuFoodHeader />
 
-      <AddFoodDialog open={dialogOpen} onOpenChange={setDialogOpen} />
+      <FoodList />
     </div>
   )
 }
