01df3a4334508cd3fc0639adcaf85289cdecec7c
MeoKool <truongsyquang1202@gmail.com>
2025-03-13 03:34:13 +0700

    feat: add file upload component for image handling; update MenuFood to include AddFoodDialog

--- src/pages/MenuFood/MenuFood.tsx
+++ src/pages/MenuFood/MenuFood.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddFoodDialog } from './components/AddFoodDialog'
+import { Button } from '@/components/ui/button'
 
 const MenuFood: React.FC = () => {
-    return (
-        <div>
+  const [dialogOpen, setDialogOpen] = useState(false)
 
+  return (
+    <div>
+      <Button onClick={() => setDialogOpen(true)}>Thêm món ăn mới</Button>
 
-
-        </div>
-    )
+      <AddFoodDialog open={dialogOpen} onOpenChange={setDialogOpen} />
+    </div>
+  )
 }
 
 export default MenuFood
